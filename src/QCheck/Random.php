74cf9c356f376b6c4551157052749617c628ba0b
Ben Peachey <potherca@gmail.com>
2015-11-29 12:47:26 +0100

    PSR-2 Compliance.
     - Adds visibility to all methods
     - Fixes PSR-2 whitespace violations

--- src/QCheck/Random.php
+++ src/QCheck/Random.php
@@ -7,39 +7,50 @@ namespace QCheck;
  * Requires gmp extension and 64 bit integer precision. The constructor will throw if
  * PHP_INT_SIZE is less than 8.
  */
-class Random {
+class Random
+{
     const ADDEND = 0xB;
     const MULTIPLIER = 0x5DEECE66D;
     private $seed;
-    static function i32($long) {
+    public static function i32($long)
+    {
         return $long << 32 >> 32;
     }
-    static function rshiftu($a, $b) {
-        if($b == 0) return $a;
+    public static function rshiftu($a, $b)
+    {
+        if ($b == 0) {
+            return $a;
+        }
         return ($a >> $b) & ~(1 << 63 >> ($b - 1));
     }
-    static function mask($val) {
+    public static function mask($val)
+    {
         return $val & ((1 << 48) - 1);
     }
-    function __construct($seed = null) {
+    public function __construct($seed = null)
+    {
         if (PHP_INT_SIZE < 8) {
             throw new \RuntimeException('64 bit integer precision required');
         }
         $this->setSeed($seed !== null ? $seed : intval(1000 * microtime(true)));
     }
-    function setSeed($seed) {
+    public function setSeed($seed)
+    {
         $this->seed = self::mask($seed ^ self::MULTIPLIER);
     }
-    protected function next($bits) {
+    protected function next($bits)
+    {
         $temp = gmp_intval(gmp_mul($this->seed, self::MULTIPLIER));
         $this->seed = self::mask($temp + self::ADDEND);
         return self::i32(self::rshiftu($this->seed, (48 - $bits)));
     }
-    function nextDouble() {
+    public function nextDouble()
+    {
         return (($this->next(26) << 27) + $this->next(27))
             / (double)(1 << 53);
     }
-    function nextInt() {
+    public function nextInt()
+    {
         return $this->next(32);
     }
 }
--- src/QCheck/RoseTree.php
+++ src/QCheck/RoseTree.php
@@ -8,35 +8,42 @@ namespace QCheck;
  *
  * @package QCheck
  */
-class RoseTree {
+class RoseTree
+{
     private $root;
     private $children;
 
-    function __construct($root, $children) {
+    public function __construct($root, $children)
+    {
         $this->root = $root;
         $this->children = $children;
     }
 
-    static function pure($val) {
+    public static function pure($val)
+    {
         return new self($val, []);
     }
 
-    function getRoot() {
+    public function getRoot()
+    {
         return $this->root;
     }
 
-    function getChildren() {
+    public function getChildren()
+    {
         return $this->children;
     }
 
-    function fmap(callable $f) {
+    public function fmap(callable $f)
+    {
         return new self(
             call_user_func($f, $this->root),
             FP::rgen(FP::map(FP::method('fmap', $f), $this->children))
         );
     }
 
-    function join() {
+    public function join()
+    {
         $innerRoot = $this->root->getRoot();
         $innerChildren = $this->root->getChildren();
         $newChildren = FP::rgen(FP::concat(
@@ -46,26 +53,31 @@ class RoseTree {
         return new self($innerRoot, $newChildren);
     }
 
-    function bind(callable $f) {
+    public function bind(callable $f)
+    {
         return $this->fmap($f)->join();
     }
 
-    function filter(callable $pred) {
+    public function filter(callable $pred)
+    {
         return new self(
             $this->root,
             FP::rgen(FP::map(
-                function(self $child) use ($pred) {
+                function (self $child) use ($pred) {
                     return $child->filter($pred);
                 },
                 FP::filter(
-                    function(self $child) use ($pred) {
+                    function (self $child) use ($pred) {
                         return call_user_func($pred, $child->getRoot());
                     },
-                    $this->children)))
+                    $this->children
+                )
+            ))
         );
     }
 
-    static private function permutations(array $roses) {
+    private static function permutations(array $roses)
+    {
         foreach ($roses as $index => $rose) {
             foreach ($rose->children as $child) {
                 yield FP::assoc($roses, $index, $child);
@@ -73,34 +85,53 @@ class RoseTree {
         }
     }
 
-    static function zip(callable $f, $roses) {
+    public static function zip(callable $f, $roses)
+    {
         return new self(
-            call_user_func_array($f,
-                FP::realize(FP::map(FP::method('getRoot'), $roses))),
-            FP::rgen(FP::map(FP::partial([__CLASS__, 'zip'], $f),
-                FP::rgen(self::permutations($roses))))
+            call_user_func_array(
+                $f,
+                FP::realize(FP::map(FP::method('getRoot'), $roses))
+            ),
+            FP::rgen(FP::map(
+                FP::partial(
+                    [__CLASS__,
+                    'zip'],
+                    $f
+                ),
+                FP::rgen(self::permutations($roses))
+            ))
         );
     }
 
-    static private function remove(array $roses) {
+    private static function remove(array $roses)
+    {
         return FP::concat(
-            FP::map(function($index) use ($roses) {
+            FP::map(function ($index) use ($roses) {
                 return FP::excludeNth($index, $roses);
             }, array_keys($roses)),
             FP::rgen(self::permutations($roses))
         );
     }
 
-    static function shrink(callable $f, $roses) {
+    public static function shrink(callable $f, $roses)
+    {
         $roses = FP::realize($roses);
         if (empty($roses)) {
             return self::pure(call_user_func($f));
         } else {
             return new self(
-                call_user_func_array($f,
-                    FP::realize(FP::map(FP::method('getRoot'), $roses))),
-                FP::rgen(FP::map(FP::partial([__CLASS__, 'shrink'], $f),
-                    self::remove($roses)))
+                call_user_func_array(
+                    $f,
+                    FP::realize(FP::map(FP::method('getRoot'), $roses))
+                ),
+                FP::rgen(FP::map(
+                    FP::partial(
+                        [__CLASS__,
+                        'shrink'],
+                        $f
+                    ),
+                    self::remove($roses)
+                ))
             );
         }
     }
